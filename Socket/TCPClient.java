740fa590d98c810f2e9484d38b1ccb81bc6911a3
Vawnndv <dinhvanguyen810@gmail>
2022-12-20 20:58:03 +0700

    Watching folder and sent status of folder to Server

--- Socket/TCPClient.java
+++ Socket/TCPClient.java
@@ -2,6 +2,12 @@ package Socket;
 
 import java.io.*;
 import java.net.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 
 public class TCPClient
 {
@@ -19,26 +25,82 @@ public class TCPClient
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
 
             String sentMessage="";
-            String receivedMessage;
-
+            String receivedMessage = "";
             System.out.println("Talking to Server");
+
+            String path = "D:\\testChange";
+            System.out.println("Watching directory for changes");
+            // STEP1: Create a watch service
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+
+            // STEP2: Get the path of the directory which you want to monitor.
+            Path directory = Path.of(path);
+
+            // STEP3: Register the directory with the watch service
+            WatchKey watchKey = directory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
+                    StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
 
             do
             {
-                DataInputStream din=new DataInputStream(System.in);
-                sentMessage=din.readLine();
-                bw.write(sentMessage);
-                bw.newLine();
-                bw.flush();
 
-                if (sentMessage.equalsIgnoreCase("quit"))
-                    break;
-                else
-                {
-                    receivedMessage=br.readLine();
-                    System.out.println("Received : " + receivedMessage);
+//                DataInputStream din=new DataInputStream(System.in);
+//                sentMessage=din.readLine();
+//                bw.write(sentMessage);
+//                bw.newLine();
+//                bw.flush();
+
+                for (WatchEvent<?> event : watchKey.pollEvents()) {
+                    sentMessage = "";
+                    // STEP5: Get file name from even context
+                    WatchEvent<Path> pathEvent = (WatchEvent<Path>) event;
+
+                    Path fileName = pathEvent.context();
+
+                    // STEP6: Check type of event.
+                    WatchEvent.Kind<?> kind = event.kind();
+                    File file = new File (path + "\\" + fileName);
+                    boolean isDirectory = file.isDirectory();
+                    boolean isFile = file.isFile();
+                    // STEP7: Perform necessary action with the event
+                    if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+                        if (isFile)
+                            sentMessage = "A new file is created : " + fileName;
+                        else if (isDirectory)
+                            sentMessage = "A new folder is created : " + fileName;
+                    }
+
+                    if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
+//                        if (isFile)
+//                            System.out.println("A file has been deleted: " + fileName);
+//                        else if (isDirectory)
+//                            System.out.println("A folder has been deleted: " + fileName);
+                        sentMessage = "A file/folder has been deleted: " + fileName;
+                    }
+                    if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+                        if (isFile)
+                            sentMessage = "A file has been modified: " + fileName;
+                        else if (isDirectory)
+                            sentMessage = "A folder has been modified: " + fileName;
+                    }
+                    bw.write(sentMessage);
+                    bw.newLine();
+                    bw.flush();
+                    System.out.println("Sent Server: " + sentMessage);
+
+                    if (receivedMessage.equalsIgnoreCase("quit"))
+                        break;
+                    else
+                    {
+                        receivedMessage=br.readLine();
+                        System.out.println("Received : " + receivedMessage);
+                    }
                 }
 
+                // STEP8: Reset the watch key everytime for continuing to use it for further event polling
+                boolean valid = watchKey.reset();
+                if (!valid) {
+                    break;
+                }
             }
             while(true);
 
--- Socket/TCPServer.java
+++ Socket/TCPServer.java
@@ -45,9 +45,9 @@ public class TCPServer extends Thread{
                 }
                 else
                 {
-                    System.out.println("Send client ("+ port + "): ");
-                    DataInputStream din=new DataInputStream(System.in);
-                    String k=din.readLine();
+//                    System.out.println("Send client ("+ port + "): ");
+//                    DataInputStream din=new DataInputStream(System.in);
+                    String k="Received";
                     bw.write(k);
                     bw.newLine();
                     bw.flush();
@@ -60,17 +60,4 @@ public class TCPServer extends Thread{
             throw new RuntimeException(e);
         }
     }
-
-    public static void main(String arg[]) throws IOException {
-        ServerSocket s = new ServerSocket(3200);
-        do {
-            System.out.println("Waiting for a Client");
-            Socket ss= s.accept(); //synchronous
-            System.out.println("Talking to client");
-            System.out.println(ss.getPort());
-            TCPServer ts = new TCPServer(s, ss);
-            ts.start();
-        } while (true);
-    }
 }
-
